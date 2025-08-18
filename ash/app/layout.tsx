3128bee89bbec021dab783508620c169531481e2
deon <ash.dawn.lacher@gmail.com>
2025-08-18 08:54:26 +0100

    build fix

--- ash/app/layout.tsx
+++ ash/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MobileOnly from "./components/MobileOnly";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Savy Encryption",
@@ -46,7 +35,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         <MobileOnly>{children}</MobileOnly>
       </body>
